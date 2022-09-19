6cc5317473dd008c99a8a1e4e9445db1aa1ec525
cristianokituxi <cristianoquichindo2@gmail.com>
2022-09-19 15:44:36 -0400

    feat:add informations

--- src/components/Sobre-clinica/index.jsx
+++ src/components/Sobre-clinica/index.jsx
@@ -4,28 +4,39 @@ import img2 from "../images/clinica2.jpg"
 import img3 from "../images/clinica3.jpeg"
 import img4 from "../images/clinica4.jpeg"
 
-import"./style.scss"
+import "./style.scss"
 const SobreAcl = () => {
   return (
     <>
       <div className="conteiner d-flex justify-content-around">
         <div className="d-flex justfy-content-between">
-           <div className="flex p-3">
-              <div className="conteiner-img">
-                  <img src={img1} alt="..."  className="img-clinica img-thumbnail"></img>
-                  <img src={img2} alt="..."  className="img-clinica img-thumbnail"></img>
-                </div>
-                <div>
-                  <img src={img3} alt="..."  className="img-clinica img-thumbnail"></img>
-                  <img src={img4} alt="..."  className="img-clinica img-thumbnail"></img>
-                </div>
+          <div className="flex p-3">
+            <div className="conteiner-img">
+              <img src={img1} alt="..." className="img-clinica img-thumbnail"></img>
+              <img src={img2} alt="..." className="img-clinica img-thumbnail"></img>
+            </div>
+            <div>
+              <img src={img3} alt="..." className="img-clinica img-thumbnail"></img>
+              <img src={img4} alt="..." className="img-clinica img-thumbnail"></img>
             </div>
           </div>
-            <div className="d-block col-4 pl-2" >
-              <h1 className="text-clinica text-align-center">A CLINÍCA</h1>
-              <p class="text-justify-center text-dr">Ambitioni dedisse scripsisse iudicaretur. Cras mattis iudicium purus sit amet fermentum. Donec sed odio operae, eu vulputate felis rhoncus. Praeterea iter est quasdam res quas ex communi. At nos hinc posthac, sitientis piros Afros. Petierunt uti sibi concilium totius Galliae in diem certam indicere. Cras mattis iudicium purus sit amet fermentum dedisse scripsisse iudicaretur. Cras mattis iudicium purus sit amet fermentum. Donec sed odio operae, eu vulputate felis rhoncus. Praeterea iter est quasdam res quas ex communi. At nos hinc posthac, sitientis piros Afros. Petierunt uti sibi concilium totius Galliae in diem certam indicere. Cras mattis iudicium purus sit amet fermentum dedisse scripsisse iudicaretur. Cras mattis iudicium purus sit amet fermentum. Donec sed odio operae, eu vulputate felis rhoncus. Praeterea iter est quasdam res quas ex communi. At nos hinc posthac, sitientis piros Afros. Petierunt uti sibi concilium totius Galliae in diem certam indicere. Cras mattis iudicium purus sit amet fermentum.</p>
-            </div>
-          </div>
+        </div>
+        <div className="d-block col-4 pl-2 text-center" >
+          <h1 className="text-clinica text-center">SOBRE A CLINÍCA</h1>
+          <h4 className="text-clinica text-align-center">Missão</h4>
+          <p class="text-justify-center text-dr">Oferecer tratamento Oftamológico com que existe de mais moderno e actualizada na oftamoogia mundial</p>
+          <h4 className="text-clinica text-align-center">Visão</h4>
+          <p class="text-justify-center text-dr">Ser o seu oftamologista de confiança assim como da sua familia,oferecendo o que existe de mais moderno para sua saúde visual. </p>
+          <h4 className="text-clinica text-align-center">Valores</h4>
+          <p class="text-justify-center text-dr">Cuidar do nosso paciente como gostaríamos que cuidasse dos nossos pais e filho;
+            estarm sempre atualizado;
+            <br />
+            Transparência com o paciente;
+            <br />
+            etica
+          </p>
+        </div>
+      </div>
 
     </>
   );
