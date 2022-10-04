87238e4be8f19edd027f8d6767151471824fff73
cristianokituxi <cristianoquichindo2@gmail.com>
2022-10-04 18:28:02 -0400

    feat:change  files in components

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -45,33 +45,6 @@ const Menu = () => {
           </li>
         </ul>
       </nav>
-      {/* <nav className="navbar navbar-expand-sm navbar-light bg-light ">
-      <div class="p-2 bd-highlight">
-      <Link className="navbar-brand" to="/">
-       <img src={logo}  width="30" height="30" class="d-inline-block align-top" alt=""></img>
-      </Link>
-      </div>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="nav navbar-nav navbar-right">
-          <li className="nav-item">
-            <Link to="/" activeStyle={{ color: "#9963BA" }} className="nav-link" activeClassName="active">Home</Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/about/" activeStyle={{ color: "#9963BA" }} className="nav-link" activeClassName="active">About</Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/gallery/" activeStyle={{ color: "#9963BA" }} className="nav-link" activeClassName="active">Gallery</Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/contact/" activeStyle={{ color: "#9963BA" }} className="nav-link" activeClassName="active">Contact</Link>
-          </li>
-        </ul>
-      </div>
-    </nav> */}
       <Banner />
 
     </>
