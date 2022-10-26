3ef47dff8d184ca9f45bf6e15af51baf9abcf67c
kiver <kiver@omegasistemas.net.br>
2022-10-26 16:39:24 -0400

    fix: Recover icon.png

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
   plugins: ["gatsby-plugin-sass", "gatsby-plugin-image", "gatsby-plugin-sitemap",{
     resolve: 'gatsby-plugin-manifest',
     options: {
-      "icon": "src/images/icon.png"
+      "icon": "./src/images/icon.png"
     }
   }, "gatsby-plugin-mdx", "gatsby-transformer-sharp", {
     resolve: 'gatsby-source-filesystem',
