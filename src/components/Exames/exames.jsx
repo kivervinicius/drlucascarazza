83e836b3bdc45845c4a7c4ab24591cd218c9a517
cristianokituxi <cristianoquichindo2@gmail.com>
2023-05-19 07:13:27 -0400

    add text for exames

---
+++ src/components/Exames/exames.jsx
@@ -0,0 +1,240 @@
+export const  consultas = {
+    
+    consultaDeRotina:[
+     {
+       id: 10,
+       name: 'Refratometria medida do grau dos óculos ',
+       nikName: 'Nesse exame, são realizados as medidas do grau dos óculos de forma automática (computadorizada) e manual (escolhida pelo próprio paciente). Os resultados são comparados e são prescritos o grau final do paciente.',
+       url: "https://www.medicalexpo.com/pt/prod/topcon-europe-medical/product-77876-471210.html",
+       img: img1,
+       imge2: ""
+     },
+    
+    
+     {
+       id: 12,
+       name: 'Tonometria de sopro com tecnologia Corneal response technology',
+       nikName: 'A tecnologia de medida da pressão por sopro é uma medida de não contato da pressão, ou seja, um jato de ar é jogado no olho e o aparelho mede a pressão analisando o comportamento desse jato de ar em contato com a córnea. É uma forma adicional de medir a pressão ocular que não precisa de colírios anestésicos nem corantes, por isso se torna mais confortável. Nosso tonometro de sopro ainda nos oferece uma medida adicional da pressão ocular, de acordo com a deformação da córnea nos fornecendo mais um parâmetro para nos auxiliar a entender a pressão intra ocular do paciente.',
+       url: "https://www.medicalexpo.com/pt/prod/topcon-europe-medical/product-77876-471210.html",
+       img: img2,
+       imge2: ""
+     },
+    
+     {
+       id: 11,
+       name: 'Tonometria convencional  (medida da pressão dos olhos)',
+       nikName: " A medida da pressão dos olhos é um exame fundamental para descobrir se o paciente tem glaucoma e para acompanhamento de pacientes com diagnóstico de glaucoma. Também é importante para acompanhamento de inflamações oculares, uso de medicamentos, pós operatórios (entre outras situações que podem alterar a pressão ocular). Sempre que possível, você terá sua pressão intra ocular aferida em suas visitas ao nosso consultório.",
+       url: "https://www.keelerusa.com/products/tonometry/kat-tonometer-type-r.html",
+       img: img6,
+       imge2: img7,
+    
+     },
+    
+    
+     {
+       id: 1,
+       name: 'Biomicroscopia (avaliação com biomicroscópio e lentes especiais da anatomia do segmento anterior e posterior do olho)',
+       nikName: 'Biomicroscopia (avaliação com biomicroscópio e lentes especiais da anatomia do segmento anterior e posterior do olho)',
+       url: "https://usophthalmic.com/products/hs-5000-5x",
+       img: img3,
+     },
+     {
+       id: 2,
+       name: 'Mapeamento de retina',
+       nikName: "Através desse exame, conseguimos avaliar os detalhes da retina do paciente para descobrir se existem doenças ou alterações retinianas bem como acompanhar o seu tratamento.",
+       url: "https://www.eyetec.com.br/equipamento-oftalmoscopio-binocular-indireto",
+       img: img4,
+       imge2: img5
+     },
+    
+     {
+       id: 3,
+       name: 'Teste de lentes de contato',
+       nikName: "Este é um procedimento indispensável para o paciente que usa óculos e deseja passar a usar também as lentes de contato. No dia do seu teste, faremos avaliação da sua córnea para entender a interação dela com a lente de contato proposta, lhe ensinaremos a manusear e cuidar de sua lente de contato e avaliaremos como ficam a visão e o seu conforto com a lente de contato. A partir desse procedimento que você poderá obter uma adaptação segura e saudável da sua lente de contato",
+       url: "",
+       imge2: ""
+     },
+    
+    
+     {
+       id: 4,
+       name: 'Teste de estrabismo (cover test)',
+       nikName: 'Este é um teste utilizado para avaliar se um paciente possuem algum grau de desvio ocular. É especialmente utilizado em crianças que podem ter seu desenvolvimento visual comprometido pelos estrabismos.',
+       url: ""
+     },
+    
+    
+     {
+       id: 5,
+       name: ' Teste de visão de cores',
+       url: "",
+       nikName: 'Este teste é útil para diagnosticar discromatopsias (daltonismo) bem como avaliar doenças no nervo óptico. Também utilizado em algumas avaliações pré operatórias.',
+       img: img8,
+       imge2: ""
+     },
+     {
+       id: 6,
+       name: 'Teste de estereopsia (avaliação da visão tridimensional)',
+       nikName: "Esse teste nos ajuda a avaliar qual o grau de visão tridimensional que o paciente possui..",
+       img: '',
+       imge2: ""
+     },
+    
+    
+    ]
+
+};
+
+  export const cornea = {
+    
+    corneaCatarata: [
+    
+    // /Córnea, Catarata, Ceratocone e Cirurgia Refrativa
+
+    {
+      id: 7,
+      name: 'Topografia de córnea',
+      nikName: 'A topografia de córnea é um exame que avalia a forma e curvatura da córnea do paciente. Ele é extremamente útil e amplamente utilizado para avaliar a córnea do paciente em casos de graus mais moderados a elevados, rastreio e acompanhamento de doenças da córnea (especialmente o ceratocone), pré operatório de cirurgias oculares (pterígio, catarata), acompanhamento do uso de lentes de contato, entre várias outras indicações.',
+      img: img9,
+      imge2: ""
+    },
+
+    {
+      id: 9,
+      name: ' Tomografia de córnea e Biometria Óptica (Pentacam AXL)',
+      nikName: ' A tomografia é uma evolução da topografia. Enquanto a topografia analisa apenas uma foto da sua córnea, a tomografia faz diversas fotos em posições diferentes para fazer uma reconstrução tridimensional da sua córnea. Por isso, ele consegue nos dar informações mais precisas e detalhadas da córnea dos pacientes, sendo muito importante em quase todas as indicações de topografia de córnea.Importante salientar que não são exames excludentes e sim complementares. Por utilizarem tecnologias diferentes para aquisição das imagens, seus resultados são sempre analisados em conjunto para melhor tomada de decisão sobre um caso clínico do paciente.',
+      img: img10,
+      imge2: ""
+    },
+
+    {
+      id: 8,
+      name: ' Biometria Ultra Sônica',
+      url: "https://www.apramed.com.br/v4/produtos_detalhes.php?id=249",
+      nikName: 'A biometria é a medida do tamanho do globo ocular e as distâncias entre as estruturas intra oculares (córnea, cristalino e retina). Muito utilizado em conjunto com as medidas da curvatura ocular (topografia e tomografia) para o cálculo da lente intra ocular do paciente. Também utilizamos muito a biometria para pacientes com grau mais elevado, diferença de grau entre os olhos e em alguns pré operatórios.',
+      img: img18,
+      imge2: ""
+    },
+
+    {
+      id: 14,
+      name: 'Biometria Óptica (Pentacam AXL)',
+      nikName: "Esse exame é uma evolução da biometria ultra sônica e utiliza um método mais moderno chamado de interferometria óptica para medir o tamanho do globo ocular de uma maneira mais precisa que o ultra som.",
+    },
+
+
+    {
+      id: 13,
+      name: ' Microscopia especular de córnea',
+      nikName: "Esse exame avalia a camada mais posterior da córnea: o endotélio. Essa camada é fundamental, pois é nela que se encontra as células responsáveis por manter a córnea transparente. Esse exame é muito utilizado em pré e pós operatório de cirurgias oculares, acompanhamento de usuários de lentes de contato bem como pacientes que possuem doenças específicas que acometem essa camada de células (distrofias do endotélio corneano).",
+    },
+
+
+
+    {
+      id: 23,
+      name: '  Potencial de acuidade visual (PAM)',
+      nikName: 'Exame útil para tentar nos mostrar qual potencial de visão o paciente poderia alcançar após uma determinada cirurgia ocular.',
+      img: img11,
+      imge2: ''
+    },
+
+    {
+      id: 15,
+      name: 'Potencial de acuidade visual (PAM)',
+      url: "https://visarerio.com.br/project/acuidade-potencial-macular/ "
+    },
+
+  ]
+}
+
+
+  // export const glaucoma = { 
+    
+  // } 
+  // [
+
+  //   // 1.3 Glaucoma:Todos os exames dessa seção são utilizados para avaliar suspeitos de glaucoma ou pacientes glaucomatosos. Eles são avaliados em conjunto e repetidos rotineiramente, conforme a necessidade individual de cada paciente.
+
+
+  //   {
+  //     id: 16,
+  //     name: 'Campimetria computadorizada',
+  //     nikName: "Sabidamente o glaucoma afeta o campo visual dos pacientes com essa doença. Esse exame permite avaliar o campo visual do paciente nos suspeitos e portadores de glaucoma. Também pode ser usado em outras doenças da retina, do nervo óptico e palpebrais.",
+  //     img: img12,
+  //     imge2: ""
+  //   },
+  //   {
+  //     id: 15,
+  //     name: 'Curva diária de pressão ocular',
+  //     nikName: "Nossa pressão intra ocular pode sofrer variações ao longo do dia. Nesse exame, fazemos algumas medidas da pressão ocular ao longo do dia com intuito de avaliar melhor quais seriam essas variações.",
+  //     img: img13,
+  //     imge2: ""
+  //   },
+  //   {
+  //     id: 17,
+  //     name: 'Teste de Sobrecarga Hídrica',
+  //     nikName: "Este teste tem como objetivo causar um estresse nas estruturas de drenagem do nosso olho (após ingestão de uma quantidade de água predeterminada). Logo após, algumas medidas de pressão são realizadas para avaliar o comportamento dessa pressão.",
+  //     img: img13,
+  //     imge2: ""
+  //   },
+  //   {
+  //     id: 18,
+  //     name: 'Gonioscopia',
+  //     nikName: "Esse exame visa avaliar as estruturas de drenagem do olho localizadas em um local denominado ângulo camerular que só pode ser observado através da utilização de lentes com espelhos especiais.",
+  //     img: img14,
+  //     imge2: ""
+  //   },
+  //   {
+  //     id: 19,
+  //     name: "Tomografia de Coerência Óptica",
+  //     nikName: "Esse exame tem como objetivo avaliar a camada de fibras nervosas e camadas de células ganglionares da retina, estruturas que são danificadas no glaucoma e que não são possíveis de serem avaliadas sem esse instrumento.",
+  //     img: img15,
+  //     imge2: ""
+  //   },
+  //   {
+  //     id: 20,
+  //     name: ' Retinografia',
+  //     nikName: " A retinografia é uma foto da sua retina e do nervo óptico que utilizamos para documentar o estado atual das estruturas intra oculares e comparar futuramente.  ",
+  //     img: img16,
+  //     imge2: ""
+  //   },
+  //   {
+  //     id: 21,
+  //     name: ' Paquimetria',
+  //     url: "Avalia a espessura da camada córnea que tem íntima relação com a pressão intra ocular.",
+  //     img: img17,
+  //     imge2: ""
+
+  //   },
+
+
+  // ]
+
+  // const retina = [
+  //   //  1.3 Retina:
+  //   {
+  //     id: 23,
+  //     name: ' Retinografia',
+  //     nikName: "Fotografia para documentação das alterações retinianas.",
+  //     img: img16,
+  //     imge2: ""
+  //   },
+
+  //   {
+  //     id: 22,
+  //     name: ' Angioflueresceinografia',
+  //     nikName: " Exame que avalia a retina, seus vasos sanguíneos e possíveis lesões da retina e nervo óptico através da injeção de um contraste intra venoso. Após a infusão desse contraste, são tiradas diversas fotos da retina e acompanhamos qual comportamento desse contraste e das estruturas retinianas para nos auxiliar no diagnóstico e acompanhamento das patologias oculares."
+  //   },
+  //   {
+  //     id: 24,
+  //     name: '  Oct Retina',
+  //     nikName: "O OCT de retina é um tomógrafo capaz de avaliar a retina tirando diversas fotografias da mesma e fazendo reconstruções que simulam cortes histológicos da mesma (analise de cada camada de células de retina) que nos ajudam a identificar precisamente qual parte da retina está danificada para podermos definir qual diagnóstico e qual melhor tratamento.",
+  //     img: img10,
+  //     imge2: ""
+
+  //   },
+
+
+
+  // ]
